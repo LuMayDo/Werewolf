514db066ef9dbb5dbf83a6eb11343c93c51f3239
Luiz Mayer <6317@htl.rennweg.at>
2021-04-25 19:17:52 +0200

    Add JSON config file

--- main.js
+++ main.js
@@ -1,13 +1,11 @@
 const 
     Discord = require('discord.js'),
     fs = require('fs'),
-    YAML = require('yaml')
 
 const 
     client = new Discord.Client(),
-    secrets = YAML.parse(fs.readFileSync('./config/secrets.yaml', 'utf-8'))
-    appToken = secrets.appToken,
-    prefix = '!ww'
+    { appToken } = require('./config/secrets.json'),
+    { prefix } = require('./config/config.json')
 
 client.commands = new Discord.Collection()
 
@@ -18,12 +16,17 @@ const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith(
 })
 
 
+
 client.once('ready', () => {
     console.log('Werewolf Bot online')
+
+    client.user.setActivity(`for a game to start | use ${prefix} help`, {
+        type: 'WATCHING'
+    })
 })
 
 client.on('message', message => {
-    if (!message.content.startsWith(prefix) || message.author.bot) return
+    if (!message.content.toLowerCase().startsWith(prefix) || message.author.bot) return
 
     const args = message.content.slice(prefix.length).trim().split(/ +/)
     const command = args.shift().toLowerCase()
