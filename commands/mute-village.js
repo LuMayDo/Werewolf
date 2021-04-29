383d47d48a17db19d0034145a34f0a6884ccede8
Luiz Mayer <6317@htl.rennweg.at>
2021-04-29 21:28:19 +0200

    Mute every member of a voice channel except user

--- commands/mute-village.js
+++ commands/mute-village.js
@@ -1,10 +1,14 @@
 module.exports = {
-    name: "mute-village",
-    description: "Mutes all villagers",
-    execute(message, args) {
-        const voiceChannel = message.member.voice.channel
-        if (!voiceChannel) return message.channel.send('You need to be in a voice channel to assign roles!')
+	name: 'mute-village',
+	description: 'Mutes all villagers',
+	execute(message, args) {
+		const voiceChannel = message.member.voice.channel
+		if (!voiceChannel)
+			return message.channel.send('You need to be in a voice channel to assign roles!')
 
-        const members = voiceChannel.members.filter(member => member !== message.member)
-    }
-};
+		const members = voiceChannel.members.filter(member => member !== message.member)
+		members.forEach(member => {
+			member.voice.setMute(true, 'You went to sleep')
+		})
+	},
+}
